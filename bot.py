932495f368395f3d2ef54364090691c6827c2b6b
pierpan2 <93636985+pierpan2@users.noreply.github.com>
2023-12-01 00:41:01 -0800

    Update bot.py (#1)
    add !sub   !unsub   !list

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ import logging
 import coloredlogs
 
 LAST_MESSAGE_TIME = None
+global ws
+ws = None
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 processed_hashes = deque(maxlen=hash_limit)
@@ -26,7 +28,6 @@ coloredlogs.install(level="ERROR", logger=error_logger)
 
 def start_websocket():
     global LAST_MESSAGE_TIME
-
     def on_ws_message(ws, message):
         global LAST_MESSAGE_TIME
         data = json.loads(message)
@@ -73,7 +74,7 @@ def start_websocket():
 async def manage_websocket():
     global LAST_MESSAGE_TIME
     LAST_MESSAGE_TIME = time.time()
-
+    global ws
     ws, ws_thread = start_websocket()
 
     while True:
@@ -83,7 +84,7 @@ async def manage_websocket():
 
             # Close the WebSocket
             ws.close()
-
+            ws = None
             # Check if the thread is still alive and join it if it is
             if ws_thread.is_alive():
                 ws_thread.join(
@@ -128,6 +129,241 @@ async def on_ready():
 
     status_update_loop.start()
     info_logger.info("Started status update loop")
+
+#Below are added
+# This function add new item to subs.json file
+def add_subscription(sub_type, sub_id, filename='subscriptions.json'):
+    global ws
+    # Construct the new subscription item
+    new_sub = {
+        "action": "sub",
+        "channel": f"{sub_type}:{sub_id}"
+    }
+    ws.send(json.dumps(new_sub))
+    # Read the current data from the file
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            subs = json.load(file)
+    except FileNotFoundError:
+        subs = []
+    except json.JSONDecodeError:
+        print("Error reading the subscriptions file. Is it empty or malformed?")
+        return False
+
+    # Add the new subscription to the list
+    subs.append(new_sub)
+
+    # Write the updated list back to the file
+    try:
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(subs, file, indent=4)
+        return True
+    except Exception as e:
+        print(f"Error writing to the JSON file: {e}")
+        return False
+# This function delete the item if in subs.json file
+def delete_subscription(sub_type, sub_id, filename='subscriptions.json'):
+    # Construct the subscription item to be deleted
+    global ws
+    target_sub = {
+        "action": "sub",
+        "channel": f"{sub_type}:{sub_id}"
+    }
+    
+    ws.send(json.dumps(target_sub))
+    try:
+        # Read the current data from the file
+        with open(filename, 'r', encoding='utf-8') as file:
+            subs = json.load(file)
+
+        # Check if the subscription is in the list and remove it
+        if target_sub in subs:
+            subs.remove(target_sub)
+        else:
+            print("Subscription not found in the file.")
+            return False
+
+        # Write the updated list back to the file
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(subs, file, indent=4)
+        return True
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+        return False
+    except json.JSONDecodeError:
+        print("Error reading the JSON file. Is it malformed?")
+        return False
+    except Exception as e:
+        print(f"Error handling the JSON file: {e}")
+        return False
+# !sub (char/ship/corp/system) name
+@bot.command(name='sub')
+async def sub(ctx, entity_type: str, *, entity_name: str):
+    # Check if the entity_type is valid
+    if entity_type.lower() not in ['char', 'ship', 'system', 'corp']:
+        await ctx.send('Invalid search type. Please select from **char**, **ship**, **system** or **corp**.')
+        return
+
+    url = "https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=[entity_name], headers=headers)
+
+    if response.status_code != 200:
+        await ctx.send('Failed to communicate with the EVE Online API.')
+        return
+
+    data = response.json()
+    # Check if the desired type is in the response
+    if entity_type.lower() == 'char' and 'characters' in data:
+        # Logic to handle character type
+        characters = data['characters']
+        # Further logic to display characters or use the information
+        await ctx.send(f'Character found: {", ".join([char["name"] for char in characters])}')
+        add_subscription('character', f'{characters[0]["id"]}')
+
+    elif entity_type.lower() == 'ship' and 'inventory_types' in data:
+        # Logic to handle inventory type
+        inventory_types = data['inventory_types']
+        # Further logic to display inventory types or use the information
+        await ctx.send(f'Ship found: {", ".join([item["name"] for item in inventory_types])}')
+        add_subscription('ship', f'{inventory_types[0]["id"]}')
+
+    elif entity_type.lower() == 'system' and 'systems' in data:
+        # Logic to handle inventory type
+        system = data['systems']
+        # Further logic to display inventory types or use the information
+        await ctx.send(f'System found: {", ".join([item["name"] for item in system])}')
+        add_subscription('system', f'{system[0]["id"]}')
+
+    elif entity_type.lower() == 'corp' and 'corporations' in data:
+        # Logic to handle inventory type
+        corporation = data['corporations']
+        # Further logic to display inventory types or use the information
+        await ctx.send(f'Corporation found: {", ".join([item["name"] for item in corporation])}')
+        add_subscription('corporation', f'{corporation[0]["id"]}')
+    else:
+        await ctx.send('No matching data found for the given type and name.')
+# !unsub (char/ship/corp/system) name
+@bot.command(name='unsub')
+async def unsub(ctx, entity_type: str, *, entity_name: str):
+    # Check if the entity_type is valid
+    if entity_type.lower() not in ['char', 'ship', 'system', 'corp']:
+        await ctx.send('Invalid search type. Please select from **char**, **ship**, **system** or **corp**.')
+        return
+
+    url = "https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=[entity_name], headers=headers)
+
+    if response.status_code != 200:
+        await ctx.send('Failed to communicate with the EVE Online API.')
+        return
+
+    data = response.json()
+
+    # Check if the desired type is in the response
+    if entity_type.lower() == 'char' and 'characters' in data:
+        # Logic to handle character type
+        characters = data['characters']
+        # Further logic to display characters or use the information
+        await ctx.send(f'Character found: {", ".join([char["name"] for char in characters])}')
+        delete_subscription('character', f'{characters[0]["id"]}')
+
+    elif entity_type.lower() == 'ship' and 'inventory_types' in data:
+        # Logic to handle inventory type
+        inventory_types = data['inventory_types']
+        # Further logic to display inventory types or use the information
+        await ctx.send(f'Ship found: {", ".join([item["name"] for item in inventory_types])}')
+        delete_subscription('ship', f'{inventory_types[0]["id"]}')
+
+    elif entity_type.lower() == 'system' and 'systems' in data:
+        # Logic to handle inventory type
+        system = data['systems']
+        # Further logic to display inventory types or use the information
+        await ctx.send(f'System found: {", ".join([item["name"] for item in system])}')
+        delete_subscription('system', f'{system[0]["id"]}')
+
+    elif entity_type.lower() == 'corp' and 'corporations' in data:
+        # Logic to handle inventory type
+        corporation = data['corporations']
+        # Further logic to display inventory types or use the information
+        await ctx.send(f'Corporation found: {", ".join([item["name"] for item in corporation])}')
+        delete_subscription('corporation', f'{corporation[0]["id"]}')
+    else:
+        await ctx.send('No matching data found for the given type and name.')
+# !list  list currently subscribed characters, ships, corps and systems
+@bot.command(name='list')
+async def list(ctx):
+    filename='subscriptions.json'
+    characters = []
+    corps = []
+    ships = []
+    systems = []
+
+    try:
+        # Read the current data from the file
+        with open(filename, 'r', encoding='utf-8') as file:
+            subs = json.load(file)
+
+        # Iterate through subscriptions and sort IDs into respective lists
+        for sub in subs:
+            channel = sub.get("channel", "")
+            if ':' in channel:
+                type, id = channel.split(':')
+                if type == 'character':
+                    response = requests.get(f'https://esi.evetech.net/latest/characters/{id}')
+                    if response.status_code == 200:
+                        data = response.json()
+                        name = data.get('name')
+                        if name:
+                            characters.append(name)
+
+                elif type == 'corporation':
+                    response = requests.get(f'https://esi.evetech.net/latest/corporations/{id}')
+                    if response.status_code == 200:
+                        data = response.json()
+                        name = data.get('name')
+                        if name:
+                            corps.append(name)
+                elif type == 'ship':
+                    response = requests.get(f'https://esi.evetech.net/latest/universe/types/{id}')
+                    if response.status_code == 200:
+                        data = response.json()
+                        name = data.get('name')
+                        if name:
+                            ships.append(name)
+                elif type == 'system':
+                    response = requests.get(f'https://esi.evetech.net/latest/universe/systems/{id}')
+                    if response.status_code == 200:
+                        data = response.json()
+                        name = data.get('name')
+                        if name:
+                            systems.append(name)
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+        return {}
+    except json.JSONDecodeError:
+        print("Error reading the JSON file. Is it malformed?")
+        return {}
+    except Exception as e:
+        print(f"Error handling the JSON file: {e}")
+        return {}
+    result = "**Currently subscribing - **\n\n"
+    if characters:
+        result += "**characters:**\n" + "\n".join(characters) + "\n\n"
+    if corps:
+        result += "**corporations:**\n" + "\n".join(corps) + "\n\n"
+    if ships:
+        result += "**ships:**\n" + "\n".join(ships) + "\n\n"
+    if systems:
+        result += "**systems:**\n" + "\n".join(systems) + "\n"
+
+    # Trim any extra newline characters from the end of the string
+    await ctx.send(result.strip())
+
+#End of new code
 
 
 bot.run(token)
